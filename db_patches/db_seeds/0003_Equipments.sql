d78b2fb3603f21d69df4a987645e75e9f4e3f44f
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2020-12-11 13:17:12 +0100

    equipments seed fixed

--- db_patches/db_seeds/0003_Equipments.sql
+++ db_patches/db_seeds/0003_Equipments.sql
@@ -2,13 +2,16 @@ DO
 $DO$
 BEGIN
 
-INSERT INTO public.scheduled_events(
-  booking_type, starts_at, ends_at, scheduled_by, description, proposal_booking_id, instrument_id)
-  VALUES ('MAINTENANCE', '2020-9-21 12:00:00', '2020-9-21 14:00:00', 100, NULL, NULL, 1);
-
-INSERT INTO public.scheduled_events(
-  booking_type, starts_at, ends_at, scheduled_by, description, proposal_booking_id, instrument_id)
-  VALUES ('MAINTENANCE', '2020-9-21 10:00:00', '2020-9-21 12:00:00', 101, NULL, NULL, 2);
+  INSERT INTO equipments
+      (owner_id, name, maintenance_starts_at, maintenance_ends_at, auto_accept)
+    VALUES 
+      (1, 'Available equipment 1 - no auto accept', null, null, false),
+      (1, 'Available equipment 2 - auto accept', null, null, true),
+      (1, 'Under maintenance indefinitely 1 - not started yet', '2020-09-24 08:00:00', null, false),
+      (1, 'Under maintenance indefinitely 2 - already started', '2020-09-20 08:00:00', null, false),
+      (1, 'Under maintenance 1 - not started yet', '2020-09-19 08:00:00', '2020-09-20 08:00:00', false),
+      (1, 'Under maintenance 2 - already started', '2020-09-19 08:00:00', '2020-09-30 08:00:00', false),
+      (1, 'Under maintenance 3 - finished', '2020-09-19 08:00:00', '2020-09-20 08:00:00', false);
 
 END;
 $DO$
